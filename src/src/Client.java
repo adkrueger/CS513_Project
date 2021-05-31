0949144f6a8ce1e6ea5d13c491b61163cfcdf41a
adkrueger <adkrueger@wpi.edu>
2021-05-31 00:18:25 -0400

    added Client code, will test tomorrowwww

--- src/src/Client.java
+++ src/src/Client.java
@@ -1,10 +1,57 @@
 package src;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
 
+    DataOutputStream clientOutput;
+    DataInputStream serverOutput;
+
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello, world!");
+        System.out.println("Enter a port number to connect to:");
+        Scanner scan = new Scanner(System.in);
+        int portNum = scan.nextInt();
+        System.out.println("Attempting to connect at port " + portNum);
+        try {
+            Socket socket = new Socket("127.0.0.1", portNum);
+            System.out.println("Connected! Enter your nickname for the server.");
+            DataOutputStream clientOutput = new DataOutputStream(socket.getOutputStream());
+            DataInputStream serverOutput = new DataInputStream( new BufferedInputStream(socket.getInputStream()));
+
+            // try setting up the client's nickname
+            String nickname = scan.nextLine();
+            clientOutput.writeUTF(nickname);
+            String response = serverOutput.readUTF();
+            System.out.println("sent nickname, serverOutput is: " + response);
+            while(response.equals("duplicate")) {
+                System.out.println("Server already has nickname '" + nickname + "' on record, " +
+                        "please enter a unique one.");
+                nickname = scan.nextLine();
+                clientOutput.writeUTF(nickname);
+                response = serverOutput.readUTF();
+            }
+            System.out.println("nickname successfully set to " + nickname);
+
+            // start allowing the client to write commands to the server
+            System.out.println("Beginning communications... type '!help' at any time to see valid commands.");
+            String curr_input = "";
+            while(!curr_input.equals("!disconnect")) {
+                curr_input = scan.nextLine();
+                System.out.println("input: " + curr_input + ", writing to server...");
+                clientOutput.writeUTF(nickname + " " + curr_input);
+                response = serverOutput.readUTF();
+                System.out.println("command successful, server response: " + response);
+            }
+        }
+        catch(IOException e) {
+            System.out.println("Error connecting to socket: " + e);
+        }
+
     }
 
 }
